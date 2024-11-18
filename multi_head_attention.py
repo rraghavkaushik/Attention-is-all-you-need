d597f212954f323c84b2b246ae90c21639ddf83c
Raghav Kaushik <136466980+rraghavkaushik@users.noreply.github.com>
2024-11-18 09:22:35 +0530

    Updated multiheadattention

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -1,31 +1,29 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Dense
-from keras.backend import softmax
+from keras.layers import Layer, Dense
+# from keras.backend import softmax
 from numpy import random
+
 
 class DotProductAttention(Layer):
     def __init__(self, **kwargs):
         super(DotProductAttention, self).__init__(**kwargs)
 
-    def calc(self, queries, keys, values, dk, mask = None):
+    def call(self, queries, keys, values, dk, mask = None):
         
-        key = np.matrix(key)
-
-        scores = (queries @ keys.transpose()) / np.sqrt(float(dk))
+        # key = np.matrix(key)
+        scores = tf.matmul(queries, keys, transpose_b=True) / tf.sqrt(tf.cast(dk, tf.float32))
 
         if mask is not None:
             scores += -1e9 * mask
 
-        weights = softmax(scores)
-
-        return weights @ values
+        weights = tf.nn.softmax(scores)
+        return tf.matmul(weights, values)
 
 
 class MultiheadAttention(Layer):
     def __init__(self, h, dk, dv, d_o, **kwargs):
         super(MultiheadAttention, self).__init__(**kwargs)
-
         self.attention = DotProductAttention()
         self.heads = h
         self.dk = dk
@@ -39,26 +37,26 @@ class MultiheadAttention(Layer):
 
     def reshape_tensor(self, x, heads, flag):
         if flag:
-            x = np.reshape(x, (x.shape[0], x.shape[1], heads, -1))
-            x = np.transpose(x, (0, 2, 1, 3))
+            x = tf.reshape(x, shape=(tf.shape(x)[0], tf.shape(x)[1], heads, -1))
+            x = tf.transpose(x, perm=(0, 2, 1, 3))
         else:
-            x = np.transpose(x, (0, 2, 1, 3))
-            x = np.reshape(x, (x.shape[0], x.shape[1], -1))
+            x = tf.transpose(x, perm=(0, 2, 1, 3))
+            x = tf.reshape(x, shape=(tf.shape(x)[0], tf.shape(x)[1], self.dk))
         return x
     
-    def cal(self, queries, keys, values, mask = None):
+    def call(self, queries, keys, values, mask = None):
 
         q_reshaped = self.reshape_tensor(self.W_Q(queries), self.heads, True)
 
         k_reshaped = self.reshape_tensor(self.W_K(keys), self.heads, True)
 
-        v_reshaped = self.reshape_tensor(self.W_K(values), self.heads, True)
+        v_reshaped = self.reshape_tensor(self.W_V(values), self.heads, True)
 
-        o_reshaped = self.attention(q_reshaped, k_reshaped, v_reshaped, self.dk, mask)
+        o_reshaped = self.attention(q_reshaped, k_reshaped, v_reshaped, dk = self.dk, mask = mask)
 
         output = self.reshape_tensor(o_reshaped, self.heads, False)
 
-        return self.W_o(output)
+        return self.W_O(output)
     
 
 #testing out the code
@@ -67,13 +65,13 @@ class MultiheadAttention(Layer):
     The below parameters are from the paper
 '''
 
-
+input_seq_length = 5
 h = 8 
 dk = 64  
 dv = 64 
 d_o = 512
 batch_size = 64  
-input_seq_length = 5
+
 
 queries = random.random((batch_size, input_seq_length, dk))
 keys = random.random((batch_size, input_seq_length, dk))
